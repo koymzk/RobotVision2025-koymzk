f532b4a0fdd8e39fef3b6d135aebc6e1a96cdd0a
T0RA <torapira@icloud.com>
2024-10-11 14:14:02 +0900

    add .venv to .gitignore

--- first/random_crop.py
+++ first/random_crop.py
@@ -1,10 +1,16 @@
 import cv2
+import numpy as np
 
 img_path = "./keio.png"
-
-# 画像の読み込み
-img = cv2.imread(img_path)
-
 # 画像をランダムに切り取るコードをかく
 # 画像は配列として読み込まれるので，
 # 画像の切り取りは，配列の一部を取り出せばできる．
+def random_crop(img_path):
+    # 画像の読み込み
+    img = cv2.imread(img_path)
+    
+
+
+if __name__ == "__main__":
+    img_path = "./keio.png"
+    random_crop(img_path)
