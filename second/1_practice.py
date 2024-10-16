b262335b48cfc94118520b6a02a069e7b1857083
T0RA <81543762+T0RA1107@users.noreply.github.com>
2024-10-16 23:28:07 +0900

    made answer example for 1_practice

--- second/1_practice.py
+++ second/1_practice.py
@@ -2,4 +2,31 @@
 import cv2
 import numpy as np
 
-# -----------以下記述-----------
+
+def main():
+    cap = cv2.VideoCapture(1)
+    grayscale = False
+    flip = False
+    stop = False
+
+    while True:
+        if not stop:
+            ret, frame = cap.read()
+            if grayscale:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            if flip:
+                frame = frame[:, ::-1]
+        cv2.imshow("camera", frame)
+        k = cv2.waitKey(1)
+        if k == ord("q"):
+            break
+        if k == ord("g"):
+            grayscale ^= True
+        elif k == ord("f"):
+            flip ^= True
+        elif k == ord("s"):
+            stop ^= True
+
+
+if __name__ == "__main__":
+    main()
