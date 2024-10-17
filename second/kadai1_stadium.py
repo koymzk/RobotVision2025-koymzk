bf6aa4736c5e62953a6c6659d990cae484e9807d
T0RA <torapira@icloud.com>
2024-10-17 16:00:35 +0900

    add answer for week2

--- second/kadai1_stadium.py
+++ second/kadai1_stadium.py
@@ -5,13 +5,19 @@ import cv2
 import numpy as np
 
 
+def clamp(x, a, b):
+    return min(max(x, a), b)
+
+
+# 範囲外に出れないようにする
 def main():
     # アプリ用のスタジアム、ボール画像を読みこみ
     ball_img = cv2.imread("./image_data/ball.png")
     stadium_img = cv2.imread("./image_data/stadium.png")
 
     # スタジアムの大きさを適当に変更 (二つ目の引数は(w,h)
-    stadium_img = cv2.resize(stadium_img, (1200, 700))
+    W, H = 1200, 700
+    stadium_img = cv2.resize(stadium_img, (W, H))
 
     # / ではなく // で切り捨て
     # ボールの高さ、幅の[半分](半分だから注意！！)
@@ -19,11 +25,8 @@ def main():
     ball_h, ball_w = ball_img.shape[0] // 2, ball_img.shape[1] // 2
 
     # ボールの中心位置（中心座標)を初期ではスタジアムの中心に設定
-    idx_h = stadium_img.shape[0] // 2
-    idx_w = stadium_img.shape[1] // 2
-
-    print(idx_h)
-
+    idx_h = H // 2
+    idx_w = W // 2
 
     while True:
         # スタジアムのコピーを作成
@@ -55,19 +58,75 @@ def main():
 
         # はみ出す場合は強制的に元の位置に戻す
         # ボールが覆う区間は[idx_h - ball_h, idx_h + ball_h)
-        idx_h = min(max(idx_h, ball_h), stadium_copy.shape[0] - ball_h)
-        idx_w = min(max(idx_w, ball_w), stadium_copy.shape[1] - ball_w)
+        idx_h = clamp(idx_h, ball_h, H - ball_h)
+        idx_w = clamp(idx_w, ball_w, W - ball_w)
         """
         ↑のコードは以下の条件分岐での対処と同じ
         if idx_h - ball_h < 0:
             idx_h = ball_h
-        if idx_h + ball_h >= stadium_copy.shape[0]:
-            idx_h = stadium_copy.shape[0] - ball_h
+        if idx_h + ball_h >= H:
+            idx_h = H - ball_h
         if idx_w - ball_w < 0:
             idx_w = ball_w
-        if idx_w + ball_w >= stadium_copy.shape[1]:
-            idx_w = stadium_copy.shape[1] - ball_w
+        if idx_w + ball_w >= W:
+            idx_w = W - ball_w
         """
+
+    cv2.destroyAllWindows()
+
+
+# 折り返して対処する
+def main2():
+    # アプリ用のスタジアム、ボール画像を読みこみ
+    ball_img = cv2.imread("./image_data/ball.png")
+    stadium_img = cv2.imread("./image_data/stadium.png")
+
+    # スタジアムの大きさを適当に変更 (二つ目の引数は(w,h)
+    W, H = 1200, 700
+    stadium_img = cv2.resize(stadium_img, (W, H))
+
+    # / ではなく // で切り捨て
+    # ボールの高さ、幅の[半分](半分だから注意！！)
+    # (注意!)今回ボールの大きさが H:198、W:200と両方偶数のためこれで良いが、奇数の場合は工夫が必要
+    ball_h, ball_w = ball_img.shape[0] // 2, ball_img.shape[1] // 2
+
+    # ボールの中心位置（中心座標)を初期ではスタジアムの中心に設定
+    idx_h = H // 2
+    idx_w = W // 2
+
+    while True:
+        # スタジアムのコピーを作成
+        stadium_copy = copy.deepcopy(stadium_img)
+
+        # ボールの再配置
+        for i in range(-ball_h, ball_h):
+            for j in range(-ball_w, ball_w):
+                stadium_i = (idx_h + i + H) % H
+                stadium_j = (idx_w + j + W) % W
+                stadium_copy[stadium_i][stadium_j] = ball_img[i + ball_h][j + ball_w]
+
+        # 結果画像の表示
+        cv2.imshow("output", stadium_copy)
+
+        # ボールを一回で動かす距離(ピクセル数)を決定
+        x = 10
+
+        # 終了オプション
+        k = cv2.waitKey(1)
+        if k == ord("q"):
+            break
+        if k == ord("w"):
+            idx_h -= x
+        elif k == ord("s"):
+            idx_h += x
+        elif k == ord("a"):
+            idx_w -= x
+        elif k == ord("d"):
+            idx_w += x
+
+        # はみ出す場合は反対方向から出す
+        idx_h = (idx_h + H) % H
+        idx_w = (idx_w + W) % W
 
 
     cv2.destroyAllWindows()
--- second/kadai2_circlelabeling.py
+++ second/kadai2_circlelabeling.py
@@ -3,30 +3,94 @@ import cv2
 import numpy as np
 
 
+def filtering(frame, hsvLower, hsvUpper):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # HSVからマスクを作成
+    hsv_mask = cv2.inRange(hsv, hsvLower, hsvUpper)
+    # medianblurを用いてノイズ成分を除去
+    blur_mask = cv2.medianBlur(hsv_mask, ksize=3)
+    return hsv_mask, blur_mask
+
+
+def labeling(blur_mask, src):
+    # ラベリング処理
+    nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(blur_mask)
+    # 領域(stats[:, 4])1つ以上ある場合(そのうち1つは背景)だけ処理
+    if nlabels >= 1:
+        # 面積でソート(今回は面積が上位1つの領域を利用)
+        top_idx = stats[:, 4].argsort()[-2:-1]
+        # 各領域において...
+        for i in top_idx:
+            # ターミナル上に詳細表示
+            print(
+                "[x0: {}, y0: {}, x幅: {}, y幅: {}, 面積: {}]".format(
+                    stats[i, 0], stats[i, 1], stats[i, 2], stats[i, 3], stats[i, 4]
+                )
+            )
+            # 領域の外接矩形の角座標を入手
+            x0 = stats[i, 0]
+            y0 = stats[i, 1]
+            x1 = x0 + stats[i, 2]
+            y1 = y0 + stats[i, 3]
+            gx = int(centroids[i, 0])
+            gy = int(centroids[i, 1])
+            # cv2.circleの引数はintである必要があるので注意
+            cv2.circle(src, (gx, gy), stats[i, 2] // 2, (0, 0, 255), 5)
+            cv2.circle(src, (gx, gy), 5, (0, 255, 0), -1)
+            # 領域の重心座標、サイズを表示 (引数 : 描画画像、 書き込む文字列、 書き込む座標、 フォント、 サイズ、 色、 太さ)
+            cv2.putText(
+                src,
+                "Center X: " + str(int(centroids[i, 0])),
+                (x1 - 30, y1 + 15),
+                cv2.FONT_HERSHEY_PLAIN,
+                1,
+                (0, 255, 255),
+                2,
+            )
+            cv2.putText(
+                src,
+                "Center Y: " + str(int(centroids[i, 1])),
+                (x1 - 30, y1 + 30),
+                cv2.FONT_HERSHEY_PLAIN,
+                1,
+                (0, 255, 255),
+                2,
+            )
+            cv2.putText(
+                src,
+                "Radius: " + str(int(stats[i, 2] // 2)),
+                (x1 - 30, y1 + 45),
+                cv2.FONT_HERSHEY_PLAIN,
+                1,
+                (0, 255, 255),
+                2,
+            )
+    return src
+
+
 def main():
     cap = cv2.VideoCapture(0)
-
     # 色の範囲
     # HSVRange["blue"]["lower"]で値を取り出せる
     HSVRange = {
         "blue": {"lower": np.array([100, 50, 50]), "upper": np.array([120, 255, 255])},
-        "green": {"lower": np.array([50, 50, 50]), "upper": np.array([60, 255, 255])},
+        "green": {"lower": np.array([40, 40, 40]), "upper": np.array([60, 255, 255])},
         "pink": {"lower": np.array([160, 50, 50]), "upper": np.array([170, 255, 255])},
     }
-
     # 実行
     while True:
         # -----------以下記述-----------
-
         # circle描画はURL参照→(http://labs.eecs.tottori-u.ac.jp/sd/Member/oyamada/OpenCV/html/py_tutorials/py_gui/py_drawing_functions/py_drawing_functions.html)
         # circle描画の ”引数:線幅” を -1 に設定することで塗り潰しが可能(円の中心点の描画に必要!)
-
+        ret, frame = cap.read()
+        for color in ["blue", "green", "pink"]:
+            _, blur_mask = filtering(frame, HSVRange[color]["lower"], HSVRange[color]["upper"])
+            frame = labeling(blur_mask, frame)
+        cv2.imshow("output", frame)
         # 終了オプション
         k = cv2.waitKey(1)
         if k == ord("q"):
             break
-
-
     # カメラリリース、windowの開放
     cap.release()
     cv2.destroyAllWindows()
